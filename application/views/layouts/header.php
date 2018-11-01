1aea79b21573c7f9aa24ae33aaa3d396b893ddac
Elang Surya <elangsurya19@gmail.com>
2018-11-02 02:03:44 +0700

    edit css

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -46,12 +46,9 @@
                 <ul class="nav-menu">
                     <li class="menu-active"><a href="#intro">Home</a></li>
                     <li><a href="#about">About</a></li>
-                    <li><a href="#speakers">Speakers</a></li>
                     <li><a href="#schedule">Schedule</a></li>
-                    <li><a href="#venue">Venue</a></li>
-                    <li><a href="#hotels">Hotels</a></li>
+                    <li><a href="#blogs">Blogs</a></li>
                     <li><a href="#gallery">Gallery</a></li>
-                    <li><a href="#sponsors">Sponsors</a></li>
                     <li><a href="#contact">Contact</a></li>
                     <li class="buy-tickets"><a href="#buy-tickets">Buy Tickets</a></li>
                 </ul>
