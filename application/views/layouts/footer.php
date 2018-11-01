8f981d22092a862e7e7c897e3d8fd29a649b5ce7
Elang Surya <elangsurya19@gmail.com>
2018-11-02 01:16:17 +0700

    my Labs

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,96 @@
+<!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <img src="<?php echo base_url() ?>assets/TheEvent/img/logo.png" alt="TheEvenet">
+            <p>In alias aperiam. Placeat tempore facere. Officiis voluptate ipsam vel eveniet est dolor et totam porro. Perspiciatis ad omnis fugit molestiae recusandae possimus. Aut consectetur id quis. In inventore consequatur ad voluptate cupiditate debitis accusamus repellat cumque.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Home</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">About us</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Services</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Home</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">About us</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Services</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>TheEvent</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=TheEvent
+        -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/easing/easing.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/superfish/hoverIntent.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/superfish/superfish.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/wow/wow.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/venobox/venobox.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/TheEvent/lib/owlcarousel/owl.carousel.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="<?php echo base_url() ?>assets/TheEvent/contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="<?php echo base_url() ?>assets/TheEvent/js/main.js"></script>
+</body>
+
+</html>
---
+++ application/views/layouts/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>TheEvent - Bootstrap Event Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicons -->
+    <link href="<?php echo base_url() ?>assets/TheEvent/img/favicon.png" rel="icon">
+    <link href="<?php echo base_url() ?>assets/TheEvent/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="<?php echo base_url() ?>assets/TheEvent/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="<?php echo base_url() ?>assets/TheEvent/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/TheEvent/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/TheEvent/lib/venobox/venobox.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/TheEvent/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="<?php echo base_url() ?>assets/TheEvent/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!--==========================
+    Header
+     ============================-->
+    <header id="header">
+        <div class="container">
+
+                <div id="logo" class="pull-left">
+                    <!-- Uncomment below if you prefer to use a text logo -->
+                    <!-- <h1><a href="#main">C<span>o</span>nf</a></h1>-->
+                    <a href="#intro" class="scrollto"><img src="<?php echo base_url() ?>assets/TheEvent/img/logo.png" alt="" title=""></a>
+                </div>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="#intro">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#speakers">Speakers</a></li>
+                    <li><a href="#schedule">Schedule</a></li>
+                    <li><a href="#venue">Venue</a></li>
+                    <li><a href="#hotels">Hotels</a></li>
+                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#sponsors">Sponsors</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li class="buy-tickets"><a href="#buy-tickets">Buy Tickets</a></li>
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </header><!-- #header -->
